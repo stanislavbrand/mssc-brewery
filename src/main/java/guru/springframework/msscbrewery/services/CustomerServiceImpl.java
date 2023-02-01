4a6874d5fd7b02d1bfe8c97cb430545e1cf61912
brand <brand@zensys.cz>
2023-02-01 16:59:19 +0100

    Assingment 1

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-04-20.
+ */
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder().id(UUID.randomUUID())
+                .name("Dummy Customer")
+                .build();
+    }
+}
